e7f0313efa264e806df84ad8629b01828da722d0
gabrielsarriavillada <gabrielsarriavillada@gmail.com>
2026-07-03 08:12:53 +0200

    Exclude files generated by playwright from git and lint processes

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,6 +3,9 @@ import tseslint from 'typescript-eslint';
 import eslintConfigPrettier from 'eslint-config-prettier';
 
 export default [
+  {
+    ignores: ['node_modules/**', 'playwright-report/**', 'test-results/**'],
+  },
   js.configs.recommended,
   ...tseslint.configs.recommended,
   eslintConfigPrettier,
